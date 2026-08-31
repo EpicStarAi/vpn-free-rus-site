fcc2939bbf14e88b2b234268299faeb8b29d7a1c
EpicStarAi <buchmanchik@gmail.com>
2026-08-31 21:58:56 +0300

    Add offline support for FREE RUS VPN client

---
+++ client/sw.js
@@ -0,0 +1,1 @@
+const CACHE='free-rus-vpn-v1';const ASSETS=['./','./index.html','./styles.css','./script.js','./manifest.webmanifest'];self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request))));
